88fb57e773dd57197be1dce8278d16e73c321563
alexmortelsison <alexmortelsison@gmail.com>
2024-04-01 15:29:21 +0900

    updated start_screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "images/logo.png",
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Wanna Learn Dart?",
+            style: TextStyle(
+                color: Color.fromARGB(255, 210, 240, 246), fontSize: 25),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              //
+            },
+            icon: const Icon(
+              Icons.arrow_right_alt_rounded,
+            ),
+            label: const Text(
+              "Start",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
