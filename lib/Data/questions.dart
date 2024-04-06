349c66cf1a2afb7c35f923823840f05d06499d9f
alexmortelsison <alexmortelsison@gmail.com>
2024-04-06 16:24:16 +0900

    updated UI

---
+++ lib/Data/questions.dart
@@ -0,0 +1,31 @@
+import 'package:quiz_app/Models/quiz_question.dart';
+
+var questions = [
+  QuizQuestion(
+    "Which species of Brown Bear is not extinct?",
+    [
+      "Syrian Brown Bear",
+      "California Grizzly Bear",
+      "Atlas Bear",
+      "Mexican Grizzly Bear",
+    ],
+  ),
+  QuizQuestion(
+    "How many rooms are there, not including the hallways and the set of stairs, in the board game 'Clue'?",
+    [
+      "9",
+      "1",
+      "6",
+      "10",
+    ],
+  ),
+  QuizQuestion(
+    "In 'Avatar: The Last Airbender&quot', which element does Aang begin to learn after being defrosted?",
+    [
+      "Water",
+      "Air",
+      "Fire",
+      "Earth",
+    ],
+  ),
+];
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:quiz_app/quiz.dart';
-import 'package:quiz_app/start_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
